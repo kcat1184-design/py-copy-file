3747fc334e8040aaacea5a200f5f4cd5cbe609ee
mac <mac@MacBook-Air-mac.local>
2026-02-28 12:08:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file = parts[1]
+    destination_file = parts[2]
+
+    if source_file == destination_file:
+        return
+    try:
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"Error: Source file {source_file} not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
