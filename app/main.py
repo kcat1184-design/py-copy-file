2598cf642cc74aadb2df545bba7404af736d6f0b
mac <mac@MacBook-Air-mac.local>
2026-02-28 12:13:17 +0200

    New

--- app/main.py
+++ app/main.py
@@ -13,7 +13,6 @@ def copy_file(command: str) -> None:
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
             file_out.write(file_in.read())
-    except FileNotFoundError:
-        print(f"Error: Source file {source_file} not found.")
+
     except Exception as e:
         print(f"An error occurred: {e}")
